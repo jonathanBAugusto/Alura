5fa7a53c61be5c02d8ed61698547bf7c2b0cce5d
jonathanBAugusto <jonathanbersot21@gmail.com>
2019-10-07 18:02:18 -0300

    Comitado

---
+++ FormacaoPhp/PHPAvancOOP/src/exceptions/OperacaoNaoRealizadaException.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace src\exceptions;
+
+class OperacaoNaoRealizadaException extends \Exception{
+    public function __construct($mensagem, $codigo, $ex)
+    {
+        parent::__construct($mensagem, $codigo, $ex);
+    }
+
+    public function __toString()
+    {
+        return $this->getCode().':'.$this->getMessage();
+    }
+}
