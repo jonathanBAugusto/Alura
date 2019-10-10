755a6c0404fcea52d9784f28266c05163fd111a7
jonathanBAugusto <jonathanbersot21@gmail.com>
2019-10-10 18:02:26 -0300

    Aula 4 e 5

---
+++ FormacaoCSharp/FormacaoCSharp/CS_4_ConversoesEOutrosTiposNumericos/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CS_4_ConversoesEOutrosTiposNumericos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Executando o projeto 4");
+
+            double salario;
+            salario = 1200.50;
+
+            int salarioEmInteiro;
+            salarioEmInteiro = (int)salario;
+
+            Console.WriteLine(salarioEmInteiro);
+
+            long idade;
+            idade = 13000000000;
+            Console.WriteLine(idade);
+
+            //Limite máximo no Short
+            short quantidadeProdutos;
+            quantidadeProdutos = 32767;
+            Console.WriteLine(quantidadeProdutos);
+
+            float altura = 1.80f;
+            Console.WriteLine(altura);
+            Console.ReadLine();
+        }
+    }
+}
